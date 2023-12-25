21b9c6298b67d4dc3bbf4de7c56fdb5cb42a6ba7
buzz_404 <nitkkree@gmail.com>
2023-12-26 01:26:59 +0530

    google auth

--- src/components/AuthForm/GoogleAuth.jsx
+++ src/components/AuthForm/GoogleAuth.jsx
@@ -1,5 +1,4 @@
 import { Flex, Image, Text } from "@chakra-ui/react";
-import React from "react";
 import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 import { auth, firestore } from "../../firebase/firebase";
 import useShowToast from "../../hooks/useShowToast";
@@ -7,9 +6,10 @@ import useAuthStore from "../../store/authStore";
 import { doc, getDoc, setDoc } from "firebase/firestore";
 
 const GoogleAuth = ({ prefix }) => {
-  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const [signInWithGoogle, , , error] = useSignInWithGoogle(auth);
   const showToast = useShowToast();
   const loginUser = useAuthStore((state) => state.login);
+
   const handleGoogleAuth = async () => {
     try {
       const newUser = await signInWithGoogle();
@@ -19,23 +19,24 @@ const GoogleAuth = ({ prefix }) => {
       }
       const userRef = doc(firestore, "users", newUser.user.uid);
       const userSnap = await getDoc(userRef);
+
       if (userSnap.exists()) {
-        //login user
+        // login
         const userDoc = userSnap.data();
         localStorage.setItem("user-info", JSON.stringify(userDoc));
         loginUser(userDoc);
       } else {
-        //sign-up user
+        // signup
         const userDoc = {
           uid: newUser.user.uid,
           email: newUser.user.email,
           username: newUser.user.email.split("@")[0],
           fullName: newUser.user.displayName,
           bio: "",
-          profilePicUrl: newUser.user.photoURL,
+          profilePicURL: newUser.user.photoURL,
           followers: [],
           following: [],
-          post: [],
+          posts: [],
           createdAt: Date.now(),
         };
         await setDoc(doc(firestore, "users", newUser.user.uid), userDoc);
@@ -46,15 +47,16 @@ const GoogleAuth = ({ prefix }) => {
       showToast("Error", error.message, "error");
     }
   };
+
   return (
     <Flex
       alignItems={"center"}
       justifyContent={"center"}
       cursor={"pointer"}
       onClick={handleGoogleAuth}>
-      <Image src="/google.png" w={5} alt="google logo" />
-      <Text mx={2} color={"blue.500"}>
-        {prefix} in with Google
+      <Image src="/google.png" w={5} alt="Google logo" />
+      <Text mx="2" color={"blue.500"}>
+        {prefix} with Google
       </Text>
     </Flex>
   );
